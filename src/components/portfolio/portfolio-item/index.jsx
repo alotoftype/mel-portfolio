bd7f7610f2ae90dc75183ffc7ebede0524c576f9
jowelht <jowel@hastechit.com>
2022-09-01 17:20:27 +0600

    react-lightgallery issue fixed

--- src/components/portfolio/portfolio-item/index.jsx
+++ src/components/portfolio/portfolio-item/index.jsx
@@ -1,27 +1,23 @@
 import PropTypes from "prop-types";
-import { LightgalleryItem, LightgalleryProvider } from "react-lightgallery";
+import { LightgalleryItem } from "react-lightgallery";
 import { Link } from "react-router-dom";
 
 const PortfolioItem = ({ portfolio }) => {
     return (
         <div className="single-portfolio">
-            <LightgalleryProvider>
-                <LightgalleryItem
-                    group="any"
-                    src={process.env.PUBLIC_URL + portfolio.homeImage}
-                >
-                    <div className="thumbnail">
-                        <div className="overlay">
-                            <img
-                                src={
-                                    process.env.PUBLIC_URL + portfolio.homeImage
-                                }
-                                alt="portfolio"
-                            />
-                        </div>
+            <LightgalleryItem
+                group="any"
+                src={process.env.PUBLIC_URL + portfolio.homeImage}
+            >
+                <div className="thumbnail">
+                    <div className="overlay">
+                        <img
+                            src={process.env.PUBLIC_URL + portfolio.homeImage}
+                            alt="portfolio"
+                        />
                     </div>
-                </LightgalleryItem>
-            </LightgalleryProvider>
+                </div>
+            </LightgalleryItem>
             <div className="content">
                 <h3 className="title">
                     <Link
--- src/containers/global/portfolio/index.jsx
+++ src/containers/global/portfolio/index.jsx
@@ -1,3 +1,4 @@
+import { LightgalleryProvider } from "react-lightgallery";
 import PortfolioFilter from "../../../components/portfolio/portfolio-filter";
 import PortfolioItem from "../../../components/portfolio/portfolio-item";
 import PortfolioData from "../../../data/portfolio.json";
@@ -26,17 +27,21 @@ const PortfolioContainer = () => {
                 </div>
                 <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 portfolio-list mb-n30">
                     <div className="col resizer"></div>
-                    {PortfolioData &&
-                        PortfolioData.map((portfolio) => (
-                            <div
-                                key={portfolio.id}
-                                className={`col masonry-grid mb-30 ${portfolio.categories
-                                    .map((cat) => slugify(cat))
-                                    .join(" ")}`}
-                            >
-                                <PortfolioItem portfolio={portfolio} />
-                            </div>
-                        ))}
+
+                    <LightgalleryProvider>
+                        {PortfolioData &&
+                            PortfolioData.map((portfolio) => (
+                                <div
+                                    key={portfolio.id}
+                                    group={`any`}
+                                    className={`col masonry-grid mb-30 ${portfolio.categories
+                                        .map((cat) => slugify(cat))
+                                        .join(" ")}`}
+                                >
+                                    <PortfolioItem portfolio={portfolio} />
+                                </div>
+                            ))}
+                    </LightgalleryProvider>
                 </div>
 
                 <div className="row">
